112ac14268aa178d3728552dc59e737d2d1043d1
Nicolas Francisquelo Tacca <nfrancisquelo2@gmail.com>
2023-01-21 14:17:24 +0100

    Backend refactor to use betdex events category endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import { Environment, MessageSendRequest } from "./types";
 import { markets } from "./markets/routes";
-import { getMarketCard, getMarketsAddresses } from "./markets/service";
+import { getMarketCard, updateMarketsAndSendBatch } from "./markets/service";
 import * as marketModel from "./markets/model";
 import { OUTCOME_IMAGES } from "./constants";
 const app = new Hono();
@@ -11,20 +11,10 @@ app.get("/", (c) => c.text("Tinbet API"));
 app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
 app.route("/markets", markets);
 
-const updateMarkets = async (env: Environment) => {
-  const marketAddresses: string[] = await getMarketsAddresses(env);
-  await marketModel.updateMarketAddresses(env.MARKETS, marketAddresses);
-  const batch: MessageSendRequest[] = marketAddresses.map((value) => ({
-    body: JSON.stringify(value),
-  }));
-  // @ts-ignore
-  await env.POPULATE_MARKETS.sendBatch(batch);
-};
-
 export default {
   fetch: app.fetch,
   scheduled: async (event, env: Environment, ctx: ExecutionContext) => {
-    ctx.waitUntil(updateMarkets(env));
+    ctx.waitUntil(updateMarketsAndSendBatch(env));
   },
   async queue(batch: MessageBatch, env: Environment, ctx: ExecutionContext) {
     for (const message of batch.messages) {
